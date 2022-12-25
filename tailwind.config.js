3bf2dc0b0d27198744a696ef0dd7a5674eac4f0e
Max Phillips <max@maxphillips.dev>
2022-12-25 20:47:51 +1100

    setup next and tailwind (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const { crimson, slate, crimsonDark, slateDark } = require("@radix-ui/colors");
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -6,6 +8,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...crimson,
+      ...slate,
+    },
     extend: {},
   },
   plugins: [],
