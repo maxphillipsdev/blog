3ec53e5f8c588e383f3c64bd2202e57425480cd2
Max Phillips <max@maxphillips.dev>
2023-01-06 00:11:26 +1100

    basic setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,6 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${jbm.variable} dark`}>
       <head />
       <body className="bg-gray-2 text-gray-11 selection:bg-crimson-9 selection:text-gray-12">
-        <div className="flex items-center justify-between p-4">
-          <span className="font-mono text-2xl text-gray-12 underline decoration-crimson-9">
-            ~/maxphillips.dev
-          </span>
-          <nav></nav>
-        </div>
         <main>{children}</main>
         <footer></footer>
       </body>
