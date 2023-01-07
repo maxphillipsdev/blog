19e7d0268759b6719fed0d4abfcfccac5c8ad512
Max Phillips <max@maxphillips.dev>
2023-01-08 00:07:03 +1100

    improve path

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -5,7 +5,7 @@ import { useDrag } from "@use-gesture/react";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ButtonHTMLAttributes, useEffect, useState } from "react";
+import { ButtonHTMLAttributes, useState } from "react";
 
 type Item = {
   label: string;
@@ -16,79 +16,80 @@ interface MenuProps {
   items?: Array<Item>;
 }
 
-const getPath = (x: number, y: number, w: number, h: number) => {
-  const anchorDistance = 200 + x * 0.5;
+const getPath = (
+  tabXPosition: number,
+  tabYPosition: number,
+  sidebarWidth: number,
+  sidebarHeight: number
+) => {
+  const anchorDistance = 200 + tabXPosition * 0.5;
   const curviness = anchorDistance - 60;
   return `M0, 
-      ${h} 
-      H0V0h${w}v 
-      ${y - anchorDistance} 
+      ${sidebarHeight} 
+      H0V0h${sidebarWidth}v 
+      ${tabYPosition - anchorDistance} 
       c0, 
       ${curviness} 
       , 
-     ${x} 
+     ${tabXPosition} 
       , 
       ${curviness} 
       , 
-     ${x} 
+     ${tabXPosition} 
       , 
       ${anchorDistance} 
-      S${w}, 
-      ${y} 
-      ,${w}, 
-      ${y + anchorDistance * 2}
+      S${sidebarWidth}, 
+      ${tabYPosition} 
+      ,${sidebarWidth}, 
+      ${tabYPosition + anchorDistance * 2}
       V
-      ${h}
+      ${sidebarHeight}
       z`;
 };
 
 export const Menu: React.FC<MenuProps> = ({ items }) => {
   // Get pathname to determine the active link
   const pathname = usePathname();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
-  // Handle menu toggle
-  const handleButtonClick = () => setOpen(!open);
+  // // Handle menu toggle
+  // const handleButtonClick = () => {
+  //   setOpen(!open);
+  // };
 
-  // Handle button movement
+  // // Handle button movement
   // const [{ btnX, btnY }, setBtn] = useSpring(() => ({
-  //   btnX: 0,
-  //   btnY: innerHeight - innerHeight * 0.72,
+  //   btnX: 10,
+  //   btnY: innerHeight * 0.1 - 15,
   //   config: {
   //     mass: 3,
   //   },
   // }));
-  // const bindButton = useDrag(({ down, movement: [mx, my] }) => {
-  //   setBtn.start({
-  //     btnX: down ? mx : 0,
-  //     btnY: down ? my : innerHeight - innerHeight * 0.72,
-  //     immediate: down,
-  //   });
-  // });
 
   // Handle clippath drawing
+  const SIDEBAR_WIDTH = innerWidth * 0.3;
+  const INACTIVE_RESTING_PATH = getPath(50, innerHeight * 0.1, 0, innerHeight);
+  const ACTIVE_RESTING_PATH = getPath(
+    0,
+    innerHeight * 0.1,
+    SIDEBAR_WIDTH,
+    innerHeight
+  );
   const [{ d }, setDValue] = useSpring(() => ({
-    d: open
-      ? getPath(0, 0, innerWidth, innerHeight)
-      : getPath(0, innerWidth / 2, innerWidth, innerHeight),
+    d: open ? ACTIVE_RESTING_PATH : INACTIVE_RESTING_PATH,
     config: {
       mass: 3,
     },
   }));
-  useEffect(() => {
-    setDValue({
-      d: getPath(48, innerHeight * 0.72, 5, innerHeight),
-    });
-  }, [setDValue]);
 
-  const bind = useDrag(({ down, movement: [mx], xy: [, my] }) => {
-    if (down) {
+  const bind = useDrag(({ down: dragging, movement: [dx, dy], xy: [x, y] }) => {
+    if (dragging) {
       setDValue({
-        d: getPath(mx, my, 10, innerHeight),
+        d: getPath(dx + 60, dy + 60, open ? SIDEBAR_WIDTH : 0, innerHeight),
       });
     } else {
       setDValue({
-        d: getPath(48, innerHeight * 0.72, 5, innerHeight),
+        d: open ? ACTIVE_RESTING_PATH : INACTIVE_RESTING_PATH,
       });
     }
   });
@@ -111,8 +112,8 @@ export const Menu: React.FC<MenuProps> = ({ items }) => {
           clipPath: `url(#menu-clip-path)`,
           WebkitClipPath: `url(#menu-clip-path)`,
         }}
-        className="fixed select-none top-0 left-0 right-0 z-40 h-full overflow-y-auto overflow-x-hidden bg-gray-1 p-4"
         {...bind()}
+        className="fixed select-none touch-none top-0 left-0 right-0 z-40 h-full overflow-y-auto overflow-x-hidden bg-gray-3 p-4"
       >
         <nav className="flex h-full w-full flex-col items-center justify-center">
           {items &&
@@ -128,7 +129,6 @@ export const Menu: React.FC<MenuProps> = ({ items }) => {
       {/* <MenuButton */}
       {/*   id="menu-button" */}
       {/*   onClick={handleButtonClick} */}
-      {/*   {...bindButton()} */}
       {/*   style={{ x: btnX, y: btnY }} */}
       {/* /> */}
     </>
