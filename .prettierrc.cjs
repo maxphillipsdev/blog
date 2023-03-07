c3911122f3a33693786a98b8d02265ea32971120
Max Phillips <max@maxphillips.dev>
2023-03-07 19:30:09 +1100

    add plugins

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,7 +2,11 @@
  * @type {import('prettier').Options}
  * */
 module.exports = {
-  plugins: [require.resolve("prettier-plugin-astro")],
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-organize-imports"),
+  ],
   overrides: [
     {
       files: "*.astro",
