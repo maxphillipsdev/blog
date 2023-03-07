3b91ac73dc31e08c21c9d2b7378a383d6c600bce
Max Phillips <max@maxphillips.dev>
2023-03-07 23:58:10 +1100

    Astro setup (#7)
    * nuke everything
    
    * astro init
    
    * some config
    
    * more setup
    
    * formatting
    
    * add react
    
    * add tailwindcss
    
    * add plugins
    
    * setup prettier and things

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: process.env.SITE_URL || "http://localhost:3000",
+  integrations: [mdx(), sitemap(), react(), tailwind()]
+});
--- lib/ghost.ts
+++
@@ -1,9 +0,0 @@
-import ghost from "@tryghost/content-api";
-
-const ghostClient = ghost({
-  url: process.env.GHOST_API_URL || "http://localhost:8080",
-  key: process.env.GHOST_API_KEY || "ghost-key",
-  version: "v5.0",
-});
-
-export default ghostClient;
