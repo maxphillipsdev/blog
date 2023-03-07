b4d9032945c81db88179a3a87ecbbd9d98f362aa
Max Phillips <max@maxphillips.dev>
2023-03-07 18:12:24 +1100

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || "http://localhost:3000",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()]
 });
