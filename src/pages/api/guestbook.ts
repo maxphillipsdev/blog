98e7478c884ad1cbbab9a672e071b4165fa426e9
Max Phillips <max@maxphillips.dev>
2023-03-26 15:42:19 +1100

    guestbook things

--- src/pages/api/guestbook.ts
+++ src/pages/api/guestbook.ts
@@ -1,9 +1,62 @@
-import { APIRoute } from "astro";
+import { PrismaClient } from "@prisma/client";
+import type { APIRoute } from "astro";
+import { z } from "astro:content";
 
-export const get: APIRoute = async ({ params, request }) => {
+const prisma = new PrismaClient();
+
+export const get: APIRoute = async () => {
+  const messages = await prisma.guestbookMessage.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 200,
+  });
+
   return {
     body: JSON.stringify({
-      message: "This was a GET!",
+      status: "Success",
+      data: messages,
+    }),
+  };
+};
+
+const ResponseSchema = z.object({
+  message: z
+    .string({
+      required_error: "Please provide a message",
+    })
+    .min(1)
+    .max(500)
+    .trim(),
+});
+
+export const post: APIRoute = async ({ request }) => {
+  const body = await request.json();
+  const result = await ResponseSchema.safeParseAsync(body);
+
+  if (!result.success) {
+    return {
+      status: 400,
+      body: JSON.stringify({
+        message: "Bad Request",
+        errors: result.error.errors,
+      }),
+    };
+  }
+
+  const { message } = result.data;
+
+  await prisma.guestbookMessage.create({
+    data: {
+      message,
+      githubID: "123",
+    },
+  });
+
+  return {
+    status: 201,
+    body: JSON.stringify({
+      status: "Success",
     }),
   };
 };
