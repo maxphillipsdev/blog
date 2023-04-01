46bfb7dc338560fa137bd805096e326805d5cfbf
Max Phillips <max@maxphillips.dev>
2023-04-01 16:43:07 +1100

    add a zoomer mode

---
+++ src/components/ZoomerMode.tsx
@@ -0,0 +1,12 @@
+const ZoomerMode = () => {
+  return (
+    <iframe
+      className="fixed bottom-0 left-0 z-50 h-[50vw] w-full"
+      src="https://5dd312fa-015f-11ea-ad56-9cb6d0d995f7.poki-gdn.com/48e861b4-7b0e-4145-8a06-960e3cd1a38c/index.html?country=AU&amp;ccpaApplies=0&amp;url_referrer=https%3A%2F%2Fpoki.com%2F&amp;tag=pg-v3.80.0&amp;site_id=3&amp;iso_lang=en&amp;poki_url=https%3A%2F%2Fpoki.com%2Fen%2Fg%2Fsubway-surfers&amp;categories=3%2C4%2C6%2C9%2C86%2C88%2C93%2C96%2C103%2C903%2C929%2C1103%2C1126%2C1137%2C1140%2C1143%2C1147%2C1156%2C1159%2C1160%2C1163%2C1179&amp;special_condition=landing&amp;game_id=5dd312fa-015f-11ea-ad56-9cb6d0d995f7&amp;game_version_id=48e861b4-7b0e-4145-8a06-960e3cd1a38c"
+      allow="autoplay; camera; focus-without-user-activation *; gamepad; keyboard-map *; xr-spatial-tracking; clipboard-write"
+      scrolling="no"
+    ></iframe>
+  );
+};
+
+export default ZoomerMode;
