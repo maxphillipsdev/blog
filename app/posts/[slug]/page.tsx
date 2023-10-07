d8e53488deb7e18242e84ec2cab9e64f5959a2b1
Max Phillips <max@maxphillips.dev>
2023-10-07 18:44:37 +1100

    contentlayer

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import { format, parseISO } from "date-fns";
+import { allPosts } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import type { MDXComponents } from "mdx/types";
+
+import Link from "next/link";
+
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  return { title: post.title };
+};
+
+const mdxComponents: MDXComponents = {
+  // Override the default <a> element to use the next/link component.
+  a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
+};
+
+const PostLayout = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  const MDXContent = useMDXComponent(post.body.code);
+
+  return (
+    <article className="mx-auto max-w-xl py-8">
+      <div className="mb-8 text-center">
+        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+      </div>
+      <MDXContent
+        className="[&>*]:mb-3 [&>*:last-child]:mb-0"
+        components={mdxComponents}
+      />
+    </article>
+  );
+};
+
+export default PostLayout;
---
+++ contentlayer.config.ts
@@ -0,0 +1,24 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+  mdx: {
+    // maybe add some remark/rehype plugins here
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+}));
+
+export default makeSource({ contentDirPath: "posts", documentTypes: [Post] });
